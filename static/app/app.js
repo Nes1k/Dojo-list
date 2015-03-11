57c8d9fa72452e55d1b0e61c907e7d03d1c2dfa2
Dominik Biel <nes1kd@gmial.com>
2015-03-11 13:56:36 +0100

    Add list view.

--- static/app/app.js
+++ static/app/app.js
@@ -1,6 +1,40 @@
 (function(){
-	angular.module('dojoApp', [])
-		.controller('baseCtrl', ['$scope', function($scope){
-			
+	angular.module('dojoApp', ['restangular'])
+		.config(['RestangularProvider',function(RestangularProvider) {
+			RestangularProvider.setRequestSuffix('/')
+		}])
+		.factory('baseList', ['Restangular', function(Restangular){
+			return Restangular.service('list');
+		}])
+		.controller('baseCtrl', ['$scope', 'baseList', function($scope, baseList){
+			$scope.data = {};
+			$scope.edited = '';
+
+			baseList.getList().then(function(list){
+				$scope.data.list = list;
+			})
+
+			$scope.setEditedList = function(id){
+				$scope.edited = id;
+			}
+
+			$scope.saveChanges = function(item){
+				item.put().then(function(){
+					$scope.edited = '';
+				})
+			}
+
+			$scope.addList = function(list){
+				baseList.post(list).then(function(new_list){
+					$scope.data.list.push(new_list)
+					$scope.list.name = '';
+				})
+			}
+
+			$scope.deleteList = function(list){
+				list.remove().then(function(){
+					$scope.data.list.splice($scope.data.list.indexOf(list), 1);
+				})
+			}
 		}])
 })()
